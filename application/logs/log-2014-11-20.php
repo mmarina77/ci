4559d359eb9cc25932db73a439ac03425630ae0c
Marina <mmelkony@sourcio.com>
2014-12-07 03:07:51 +0400

    Delete log-2014-11-20.php

--- application/logs/log-2014-11-20.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2014-11-20 15:17:34 --> Severity: Warning  --> fopen(D:/data/www/ci/uploads/2014_11_20.csv): failed to open stream: No such file or directory D:\data\www\ci\application\libraries\csv_class.php 40
